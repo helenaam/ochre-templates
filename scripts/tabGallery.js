2c27f131e55289b1e463de5c4a7831eccefda06c
Miller Prosser <mcprosse@uchicago.edu>
2020-07-29 09:21:19 -0500

    load XML document with JS

--- scripts/tabGallery.js
+++ scripts/tabGallery.js
@@ -1,3 +1,31 @@
+function loadXML(){
+  //based on https://www.w3schools.com/xml/dom_intro.asp
+  //Here's a curl command to load an xml file in terminal:
+  //curl "http://ochre.lib.uchicago.edu/ochre?uuid=e59a5d19-0ea8-49c4-a814-05c6674f0f0a" | xmllint --format - | less
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+      loadXMLresponse(this);
+    }
+  };
+  xhttp.open("GET", "http://ochre.lib.uchicago.edu/ochre?uuid=e59a5d19-0ea8-49c4-a814-05c6674f0f0a", true);
+  xhttp.send();
+  console.log(`xhttp is ${xhttp}`);
+
+  function loadXMLresponse(xml){
+    var uuids = [];
+    var xmlDoc = xml.responseXML;
+    console.log(`xmlDoc is ${xmlDoc}`);
+    var testVar = xmlDoc.getElementsByTagName("resource");
+    console.log(`testVar is ${testVar}; ${testVar.length}`);
+    for (var i=0; i < testVar.length; i++){
+      uuids.push(testVar[i].getAttribute('uuid'));
+    };
+    console.log(`uuid list is ${uuids}`);
+  }
+}
+
 function tabGallery(imgs) {
   // Get the expanded image
   var expandImg = document.getElementById("expandedImg");
