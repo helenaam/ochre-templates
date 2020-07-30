e82fb9fb5ccd3c2683f220cdfb2f7e3c61dc6c00
Helena <h.abneymcp@gmail.com>
2020-07-30 16:57:12 -0500

    added links and functionality for displaying different image hierarchies on gallery page

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,5 +1,23 @@
-// Replace the URL below with the URL for your image hierarchy
-var url = "http://ochre.lib.uchicago.edu/ochre?uuid=e59a5d19-0ea8-49c4-a814-05c6674f0f0a";
+// Replace this URL with the URL for your first image hierarchy or set
+var url1 = "http://ochre.lib.uchicago.edu/ochre?uuid=e59a5d19-0ea8-49c4-a814-05c6674f0f0a";
+// Replace this URL with the URL for your second image hierarchy or set
+var url2 = "http://ochre.lib.uchicago.edu/ochre?uuid=e59a5d19-0ea8-49c4-a814-05c6674f0f0a";
+// Replace this URL with the URL for your third image hierarchy or set
+var url3 = "http://ochre.lib.uchicago.edu/ochre?uuid=e59a5d19-0ea8-49c4-a814-05c6674f0f0a";
+
+var uuids = [];
+//var tiles = [];
+
+function loadXML(link){
+    var connect = new XMLHttpRequest();
+    connect.onreadystatechange = function(){
+	if(this.readyState == 4 && this.status == 200){
+	    loadUUIDs(this.responseXML);
+	}
+    };
+    connect.open("GET", link, true);
+    connect.send();
+}
 
 function loadUUIDs(sourceXML){
     var images = sourceXML.getElementsByTagName("resource");
@@ -23,25 +41,40 @@ function prepareArray(uuids){
     return tileSources;
 }
 
-var uuids = [];
-var connect = new XMLHttpRequest();
-connect.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-	loadUUIDs(this.responseXML);
-    }
-};
-connect.open("GET", url, true);
-connect.send();
+function showImages(uuids){
+    alert(); // For some reason, it only works when I include this line
+    var tiles = prepareArray(uuids);
 
-alert(); // For some reason, it only works when I include this line
-var tileSources = prepareArray(uuids);
+    var viewer = OpenSeadragon({
+	id: "seadragon-viewer",
+	prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
+	collectionMode: true,
+	collectionRows: 1,
+	collectionTileSize: 1024,
+	collectionTileMargin: 256,
+	showZoomControl: true,
+	showHomeControl: true,
+	showFullPageControl: true,
+	tileSources: tiles
+    });
+}
 
-var viewer = OpenSeadragon({
-    id: "seadragon-viewer",
-    prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
-    collectionMode: true,
-    collectionRows: 1,
-    collectionTileSize: 1024,
-    collectionTileMargin: 256,
-    tileSources: tileSources
-});
+var galLink1 = document.getElementById("link1");
+galLink1.onclick = function() {
+    loadXML(url1);
+    showImages(uuids);
+    return false;
+}
+var galLink2 = document.getElementById("link2");
+galLink2.onclick = function() {
+    loadXML(url2);
+    showImages(uuids);
+    return false;
+}
+var galLink3 = document.getElementById("link3");
+galLink3.onclick = function() {
+    loadXML(url3);
+    showImages(uuids);
+    return false;
+}
+
