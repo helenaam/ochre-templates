1798235d2f8f91bc2b1423fbd468ef2d91c1f131
Helena <h.abneymcp@gmail.com>
2020-08-03 15:24:25 -0500

    fixed gallery javascript code, updated styles

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,18 +1,19 @@
 // Replace this URL with the URL for your first image hierarchy or set
-var url1 = "http://ochre.lib.uchicago.edu/ochre?uuid=e59a5d19-0ea8-49c4-a814-05c6674f0f0a";
+var url1 = "http://ochre.lib.uchicago.edu/ochre?uuid=cc6a3b57-23de-f359-eed7-76a7c931b480";
 // Replace this URL with the URL for your second image hierarchy or set
 var url2 = "http://ochre.lib.uchicago.edu/ochre?uuid=e59a5d19-0ea8-49c4-a814-05c6674f0f0a";
 // Replace this URL with the URL for your third image hierarchy or set
 var url3 = "http://ochre.lib.uchicago.edu/ochre?uuid=e59a5d19-0ea8-49c4-a814-05c6674f0f0a";
 
 var uuids = [];
-//var tiles = [];
+var tiles = [];
 
 function loadXML(link){
     var connect = new XMLHttpRequest();
     connect.onreadystatechange = function(){
 	if(this.readyState == 4 && this.status == 200){
 	    loadUUIDs(this.responseXML);
+	    showImages(uuids);
 	}
     };
     connect.open("GET", link, true);
@@ -42,16 +43,17 @@ function prepareArray(uuids){
 }
 
 function showImages(uuids){
-    alert(); // For some reason, it only works when I include this line
-    var tiles = prepareArray(uuids);
+    //alert(uuids); // For some reason, it only works when I include this line
+    tiles = prepareArray(uuids);
 
     var viewer = OpenSeadragon({
 	id: "seadragon-viewer",
 	prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
 	collectionMode: true,
-	collectionRows: 1,
+	collectionRows: 3,
 	collectionTileSize: 1024,
 	collectionTileMargin: 256,
+	collectionLayout: 'vertical',
 	showZoomControl: true,
 	showHomeControl: true,
 	showFullPageControl: true,
@@ -62,19 +64,19 @@ function showImages(uuids){
 var galLink1 = document.getElementById("link1");
 galLink1.onclick = function() {
     loadXML(url1);
-    showImages(uuids);
+    //showImages(uuids);
     return false;
 }
 var galLink2 = document.getElementById("link2");
 galLink2.onclick = function() {
     loadXML(url2);
-    showImages(uuids);
+    //showImages(uuids);
     return false;
 }
 var galLink3 = document.getElementById("link3");
 galLink3.onclick = function() {
     loadXML(url3);
-    showImages(uuids);
+    //showImages(uuids);
     return false;
 }
 
