f4294e3094f92ed3190af88ed8b57544e2067dbc
Helena <h.abneymcp@gmail.com>
2020-08-22 16:28:22 -0500

    hotspot links now load in div next to image

--- scripts/hotspots.js
+++ scripts/hotspots.js
@@ -6,16 +6,35 @@ var formatted = "";
 var connect = new XMLHttpRequest();
 connect.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
+	//formatted = "";
 	clickAreas = this.responseXML.getElementsByTagName("area");
-	formatHTML(clickAreas);
+	formatHTML_div(clickAreas);
+	// formatHTML_page(clickAreas);
 	document.getElementById('imageMap').innerHTML = formatted;
     }
 };
 connect.open("GET", url, true);
 connect.send();
 
-function formatHTML(areaList){
+// Use this function if you want to visit the resulting page rather than having it load in a div
+function formatHTML_page(areaList){
+    //formatted = "";
     for(var i = 0; i < areaList.length; i++){
 	formatted = formatted + "\n<area shape=\"" + areaList[i].getAttribute('shape') + "\" coords=\"" + areaList[i].getAttribute('coords') + "\" href=\"http://pi.lib.uchicago.edu/1001/org/ochre/" + areaList[i].getAttribute('uuid') + "\">";
     }
+}
+
+// Use this function if you want the resulting page to load in a div that can be viewed alongside the image
+function formatHTML_div(areaList){
+    //formatted = "";
+    for(var i = 0; i < areaList.length; i++){
+	formatted = formatted + "\n<area shape=\"" + areaList[i].getAttribute('shape') + "\" coords=\"" + areaList[i].getAttribute('coords') + "\" onclick=\"loadResults(\'" + areaList[i].getAttribute('uuid') + "\')\">";
+    }
+}
+
+function loadResults(uuid){
+    // alert("called loadResults");
+    var link = "http://pi.lib.uchicago.edu/1001/org/ochre/" + uuid;
+    // document.getElementById("results-page").innerHTML = '<object type="text/html" data="' + link + '"></object>';
+    document.getElementById("results-page").innerHTML = '<iframe src="' + link + '" height="100%"></iframe>';
 }
